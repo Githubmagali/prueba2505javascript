5a7ae772e2e02116655191103f115a4467d127cb
Githubmagali <106199984+Githubmagali@users.noreply.github.com>
2023-06-02 19:20:17 -0300

    cambios

--- herencia2.js
+++ herencia2.js
@@ -18,14 +18,17 @@ class Moderador extends Persona{
   borrarPost(id){
     if(this.permisos.includes('borrar')){
         console.log(`El usuario ${id} ha sido borrado`);
+    } else{
+        console.log('No tiene  los permisos para borrar post');
     }
+    
   }
 }
 
 const Per= new Persona('Maria', 78, 1.80, 'medica');
 console.log(Per.obtenerPosts());
 
-const Per2= new Moderador('Mario', 56, 1.60, 'dentista',['borrar', 'editar']);
+const Per2= new Moderador('Mario', 56, 1.60, 'dentista',[ 'editar']);
 console.log(Per2.obtenerPosts());
 console.log(Per2.permisos);
 Per2.borrarPost(5);
---
+++ metodosEstaticos.js
@@ -0,0 +1,28 @@
+
+//Metodo inicializando datos: Para ello tenes que hace un nvo objeto
+
+class Usuario{
+    constructor(nombre, correo){
+        this.nombre=nombre;
+        this.correo=correo;
+    }
+
+    borrarUsuario(id_usuario){
+        console.log(`El usuario ${id_usuario} fue eliminado`);
+    }
+}
+const Us = new Usuario ('Martin', 'martin@mail.com');
+Us.borrarUsuario(1);
+
+//Metodo estatico accede a propiedades y metodos sin tener que inicializarlos
+
+class Usuario1997{
+    constructor(nombre, apellido){
+        this.nombre=nombre;
+        this.apellido=apellido;
+    }
+    static borrar(id_usuario){
+        console.log(`El usuario ${id_usuario} fue borrado`) 
+    }
+    }
+Usuario1997.borrar(3);
