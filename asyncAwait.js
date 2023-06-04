c528a962d7046d959a814d109ecf196b64b6c04c
Githubmagali <106199984+Githubmagali@users.noreply.github.com>
2023-06-04 08:57:58 -0300

    cambios

--- asyncAwait.js
+++ asyncAwait.js
@@ -2,7 +2,7 @@ const fetchPost = () => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       const posts = ["post1", "post2", "post3"];
-      const error = false;
+      const error = true;
       if (error) {
         reject("Hubo un error");
       } else {
@@ -16,11 +16,11 @@ const fetchPost = () => {
 //.catch((error)=>console.log(error));
 //console.log('Finaliza la operacion');//y mientras tanto ejecuta codigo Como este console.log
 
-const mostrarPosts = async () => {
-  try {
-    const posts = await fetchPost();
+const mostrarPosts = async () => { //async indica el retorno de la promesa se va a ejecutar pero no va a mostrar codigo hasta que se ejecute la promesa 
+  try { //contiene el codigo para ejecutarlo
+    const posts = await fetchPost(); //Await espera el retorno d ela promesa y ejecuta el codigo
     console.log(posts);
-  } catch (error) {
+  } catch (error) { //catch atrapa un error si existe en try
     console.log(error);
   }
 };
